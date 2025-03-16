0783b965ef4de17ff05bbd0c3a9b69c5601b233a
Ilya <iluxapix@gmail.com>
2025-03-16 21:32:17 +0300

    Add files via upload

---
+++ ModularConsole/ModularConsole/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using ModularLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите задание:");
+                Console.WriteLine("(4, 9, 10, 11, 16, 17, 18, 21, 22, 23, 24)\n");
+                Console.WriteLine("или напишите 'exit' для выхода из программы. ");
+                string choice = Console.ReadLine();
+
+                if (choice.ToLower() == "exit") 
+                    break;
+
+                switch (choice)
+                {
+                    case "4":
+                        Console.WriteLine("Введите a и b:");
+                        double a = double.Parse(Console.ReadLine());
+                        double b = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Максимум: {MathimaticFunctions.MaxOfThreeExpressions(a, b)}\n");
+                        break;
+
+                    case "9":
+                        Console.WriteLine("Введите массив из 10 целых чисел через пробел:");
+                        int[] array9 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                        Console.WriteLine("Введите число n:");
+                        int n = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Число {n} повторяется {MathimaticFunctions.CountOccurrences(array9, n)} раз(а)\n");
+                        break;
+
+                    case "10":
+                        Console.WriteLine("Введите строку:");
+                        string str10 = Console.ReadLine();
+                        Console.WriteLine("Введите символ:");
+                        char ch10 = Console.ReadLine()[0];
+                        Console.WriteLine($"Символ '{ch10}' встречается {MathimaticFunctions.CountCharOccurrences(str10, ch10)} раз(а)\n");
+                        break;
+
+                    case "11":
+                        Console.WriteLine("Введите массив целых чисел через пробел:");
+                        int[] array11 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                        var result = MathimaticFunctions.FindMaxAndCount(array11);
+                        Console.WriteLine($"Максимальный элемент: {result.max}, количество: {result.count}\n");
+                        break;
+
+                    case "16":
+                        Console.WriteLine("Введите размер квадратной матрицы:");
+                        int size16 = int.Parse(Console.ReadLine());
+                        int[,] matrix16 = new int[size16, size16];
+                        Console.WriteLine("Введите элементы матрицы построчно:");
+                        for (int i = 0; i < size16; i++)
+                        {
+                            int[] row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                            for (int j = 0; j < size16; j++)
+                            {
+                                matrix16[i, j] = row[j];
+                            }
+                        }
+                        MathimaticFunctions.ReplaceDiagonalWithZeros(matrix16);
+                        Console.WriteLine("Матрица после замены диагонали на нули:\n");
+                        PrintMatrix(matrix16);
+                        break;
+
+                    case "17":
+                        Console.WriteLine("Введите размер квадратной матрицы:");
+                        int size17 = int.Parse(Console.ReadLine());
+                        int[,] matrix17 = new int[size17, size17];
+                        Console.WriteLine("Введите элементы матрицы построчно:");
+                        for (int i = 0; i < size17; i++)
+                        {
+                            int[] row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                            for (int j = 0; j < size17; j++)
+                            {
+                                matrix17[i, j] = row[j];
+                            }
+                        }
+                        MathimaticFunctions.ReplaceNegativesWithOnes(matrix17);
+                        Console.WriteLine("Матрица после замены отрицательных элементов на единицы:\n");
+                        PrintMatrix(matrix17);
+                        break;
+
+                    case "18":
+                        Console.WriteLine("Введите размер матрицы (строки и столбцы):");
+                        int rows18 = int.Parse(Console.ReadLine());
+                        int cols18 = int.Parse(Console.ReadLine());
+                        int[,] matrix18 = new int[rows18, cols18];
+                        Console.WriteLine("Введите элементы матрицы построчно:");
+                        for (int i = 0; i < rows18; i++)
+                        {
+                            int[] row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                            for (int j = 0; j < cols18; j++)
+                            {
+                                matrix18[i, j] = row[j];
+                            }
+                        }
+                        Console.WriteLine($"Сумма минимальных элементов по строкам: {MathimaticFunctions.SumOfMinElements(matrix18)}\n");
+                        break;
+
+                    case "21":
+                        Console.WriteLine("Введите строку:");
+                        string str21 = Console.ReadLine();
+                        Console.WriteLine($"Реверсированная строка: {MathimaticFunctions.ReverseString(str21)}\n");
+                        break;
+
+                    case "22":
+                        Console.WriteLine("Введите строку:");
+                        string str22 = Console.ReadLine();
+                        Console.WriteLine($"Строка с добавленными символами: {MathimaticFunctions.AddSymbolsToString(str22)}\n");
+                        break;
+
+                    case "23":
+                        Console.WriteLine("Введите символ:");
+                        char ch23 = Console.ReadLine()[0];
+                        Console.WriteLine("Введите количество символов k:");
+                        int k23 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Строка из {k23} символов: {MathimaticFunctions.CreateStringFromChar(ch23, k23)}\n");
+                        break;
+
+                    case "24":
+                        Console.WriteLine("Введите массив целых чисел через пробел:");
+                        int[] array24 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                        Console.WriteLine("Введите количество элементов n:");
+                        int n24 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Сумма первых {n24} элементов: {MathimaticFunctions.SumFirstNElements(array24, n24)}\n");
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Неверный выбор. Попробуйте снова!\n");
+                        break;
+                }
+            }
+        }
+        // вывод матрицы
+        static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ModularLibrary/ModularLibrary/Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularLibrary
+{
+    public class MathimaticFunctions
+    {
+        // 7.4. Возвращает максимум из трех выражений
+        public static double MaxOfThreeExpressions(double a, double b)
+        {
+            double x = Math.Pow(Math.Cos(a + b), 2);
+            double y = Math.Sin(a + b);
+            double z = (x + y) / 2;
+
+            return Math.Max(x, Math.Max(y, z));
+        }
+
+        // 7.9. Подсчет количества повторений числа в массиве
+        public static int CountOccurrences(int[] array, int n)
+        {
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num == n) count++;
+            }
+            return count;
+        }
+
+        // 7.10. Подсчет количества вхождений символа в строке
+        public static int CountCharOccurrences(string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == ch) count++;
+            }
+            return count;
+        }
+
+        // 7.11. Поиск максимального элемента и подсчет количества максимумов
+        public static (int max, int count) FindMaxAndCount(int[] array)
+        {
+            int max = array[0];
+            int count = 1;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    count = 1;
+                }
+                else if (array[i] == max)
+                {
+                    count++;
+                }
+            }
+
+            return (max, count);
+        }
+
+        // 7.16. Замена элементов диагонали матрицы на нули
+        public static void ReplaceDiagonalWithZeros(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                matrix[i, i] = 0;
+            }
+        }
+
+        // 7.17. Замена всех отрицательных чисел в матрице на единицы
+        public static void ReplaceNegativesWithOnes(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] < 0) matrix[i, j] = 1;
+                }
+            }
+        }
+
+        // 7.18. Сумма минимальных элементов матрицы по строкам
+        public static int SumOfMinElements(int[,] matrix)
+        {
+            int sum = 0;
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                int min = matrix[i, 0];
+                for (int j = 1; j < cols; j++)
+                {
+                    if (matrix[i, j] < min) min = matrix[i, j];
+                }
+                sum += min;
+            }
+
+            return sum;
+        }
+
+        // 7.21. Получение новой строки, прочитанной с конца
+        public static string ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        // 7.22. Добавление символов в начало строки
+        public static string AddSymbolsToString(string str)
+        {
+            return "++++----" + str;
+        }
+
+        // 7.23. Формирование строки из k символов
+        public static string CreateStringFromChar(char ch, int k)
+        {
+            return new string(ch, k);
+        }
+
+        // 7.24. Суммирование первых n элементов массива
+        public static int SumFirstNElements(int[] array, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n && i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
